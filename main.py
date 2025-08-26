07acd84a2e22a9aa09c0651a9a6ebab71c7120d8
BlackArmy313 <227722544+panjihitam855@users.noreply.github.com>
2025-08-26 14:31:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
 def menu():
    # print(Style.BRIGHT + Fore.YELLOW + "[INFO!]" Fore.WHITE + "Press CTRL + C and press enter to exit!!")
     print(Style.BRIGHT + Fore.YELLOW + "[INFO!]" + Fore.BLUE + "Press CTRL + C and press enter to exit!!")
-    print(Fore.WHITE + Style.BRIGHT + "——————————————————————————————————————————————————————————————————")
+    print(Fore.WHITE + Style.BRIGHT + "════════════════════════════════════════════════════════")
     print(Fore.YELLOW + Style.BRIGHT + "Silahkan ketik 1 untuk melanjutkan...")
     print(Fore.BLUE + Style.BRIGHT + "1. DDos a website.  [1]")
     print(Fore.WHITE + Style.BRIGHT + "2. exit.            [2]")
